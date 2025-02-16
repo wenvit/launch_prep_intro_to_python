61abeb0a76ba6377ba80ee3555cdef3285ef0e88
wenvit <wendyvit@icloud.com>
2025-02-16 16:10:03 -0600

    simplified for loop by iterating over range

--- variables/exercise9.py
+++ variables/exercise9.py
@@ -1,15 +1,10 @@
 # 9. Repeat the previous question but, this time, use augmented assignment 
 # to compute the final result, one year at a time.
 
-initial_balance = 1000
+balance = 1000
 interest_rate = 0.05
 interest_multiplier = 1 + interest_rate
 
-balance = initial_balance * interest_multiplier
-print(f'Bank balance at end of year 1: {balance}')
-
-years = [2, 3, 4, 5]
-
-for year in years:
+for year in range(1, 6):
     balance *= interest_multiplier
     print(f'Bank balance at end of year {year}: {balance}')
